c596a4bbe775e712570458202b19b37c5bb4dffc
Zachary McCormick <zmccormick6@gmail.com>
2018-04-10 23:49:56 -0500

    Add Raycast Switching and Basic Game Layout

---
+++ Assets/Scripts/ZachRaycast.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZachRaycast : MonoBehaviour {
+	void Update() {
+		if (Input.GetMouseButton(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.rigidbody != null) {
+					if (hit.transform.gameObject.layer == 8) {
+						var tag = hit.transform.gameObject.tag;
+
+						if (tag == "MaxGame") {
+							GetComponent<GameControl>().MaxGame();
+						}
+						else if (tag == "EmilyGame") {
+							GetComponent<GameControl>().EmilyGame();
+						}
+						else if (tag == "LukeGame") {
+							GetComponent<GameControl>().LukeGame();
+						}
+						else if (tag == "NicoleGame") {
+							GetComponent<GameControl>().NicoleGame();
+						}
+
+						GetComponent<GameControl>().SwitchGames();
+					}
+				}
+			}
+        }
+	}
+}
