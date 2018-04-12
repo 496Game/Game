b8cd51b11f3b5062ffa2a91a74780b27b4f8e5e1
emilykikima <36427765+emilykikima@users.noreply.github.com>
2018-04-11 19:18:07 -0500

    Sound
    There are buttons that make noise and a check to see if you are pressing the correct one. I also added five tags

--- Assets/Scripts/EmilyRaycast.cs
+++ Assets/Scripts/EmilyRaycast.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EmilyRaycast : MonoBehaviour {
+
+	public char[] notes = new char[] {'d','a','b'} ;
+	public int ap = 0 ; // array pointer
 	void Update() {
-		if (Input.GetMouseButton(0)) {
+		if (Input.GetMouseButtonDown(0)) {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit)) {
                 if (hit.rigidbody != null) {
-					hit.transform.gameObject.GetComponent<Renderer>().material.color = Color.red;
-				}
-			}
-        }
-	}
+					hit.transform.gameObject.GetComponent<AudioSource>().Play();
+
+					// save the tag and compare it to the apropriete location in the notes array
+					string currentNote = hit.transform.gameObject.tag ;
+
+					if (notes [ap].ToString () == currentNote) {
+						print ("Correct");
+						ap++;
+					} else {
+						print ("Bad");
+						ap = 0;
+					} // end if else
+				} // end if
+			} // end if
+        } // end if
+	} // end Update()
 }
