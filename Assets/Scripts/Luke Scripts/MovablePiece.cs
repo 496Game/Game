d23cf0d6696f6cc3a1657f797cf4e1b673022203
nicolepnelson <nicolenelson@Nicoles-MBP-4.eau.wi.charter.com>
2018-04-18 21:23:55 -0500

    Commit Luke’s files to lever branch

---
+++ Assets/Scripts/Luke Scripts/MovablePiece.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovablePiece : MonoBehaviour {
+
+	private int x;
+	private int y;
+	private GamePiece piece;
+
+	void Awake(){
+		piece = GetComponent<GamePiece> ();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Move(int newX, int newY){
+		piece.X = newX;
+		piece.Y = newY;
+		piece.transform.localPosition = piece.GridRef.GetWorldPosition (newX, newY, .55f);
+	}
+}
