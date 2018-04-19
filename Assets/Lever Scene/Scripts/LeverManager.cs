88238dee010d294dba8f9623e6fe047b305d889c
nicolepnelson <nicolenelson@Nicoles-MacBook-Pro-4.local>
2018-04-19 17:39:06 -0500

    Revert "particles"
    This reverts commit e5bb49508ba17c700f0a76e18ce523a0e2526685.

--- Assets/Lever Scene/Scripts/LeverManager.cs
+++ Assets/Lever Scene/Scripts/LeverManager.cs
@@ -49,9 +49,6 @@ public class LeverManager : MonoBehaviour {
 
 		// loop through all of the lights to see if the level is complete
 		success = Kuzco();
-		if (success) {
-			EmperorsNewGroove ();
-		}
 		Player.GetComponent<GameControl>().NicoleGameComplete = success;
 	}
 
@@ -75,20 +72,5 @@ public class LeverManager : MonoBehaviour {
 		}
 
 		return true;
-	}
-
-	// Method:      EmperorsNewGroove
-	// Parameter:   none
-	// Return:      None
-	// Description: Upon completion of the puzzle, this method
-	//              will call the animator of all lights and 
-	//              switch their color.
-	void EmperorsNewGroove() {
-		for (int i = 0; i < lights.Length; i++) {
-			anim = lights [i].GetComponent<Animator> ();
-
-			anim.SetBool ("success", true);
-		}
-		anim.SetBool ("success", false);
 	}
 }
