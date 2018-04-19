c6d00ed23152f0afd45b342d4f253ee561869b5e
Luke Lamberg <luke.lamberg@yahoo.com>
2018-04-18 21:13:15 -0500

    LukeGame
    Has win condition in the GM script, is bool GAMEOVER

---
+++ Assets/Scripts/Luke Scripts/ColorPiece.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPiece : MonoBehaviour {
+
+	public enum ColorType{
+		YELLOW,
+		PURPLE,
+		RED,
+		BLUE,
+		GREEN,
+		PINK,
+		ANY,
+		COUNT
+	};
+
+	[System.Serializable]
+	public struct colorPiece
+	{
+		public ColorType color;
+		public GameObject thePiece;
+	}
+	private GameObject thePiecee;
+	public colorPiece[] colorPieces;
+	private ColorType color;
+
+	public int numColors{
+		get{return colorPieces.Length;}
+	}
+
+	private ColorType Color{
+		get{ return Color; }
+		set{ SetColor(value);}
+	}
+
+	private Dictionary<ColorType, GameObject> colorPieceDict;
+	// Use this for initialization
+
+	void Awake(){
+		thePiecee = gameObject;
+		//Material mat = thePiecee.AddComponent<Material> () as Material;
+		Destroy (thePiecee.GetComponent<Rigidbody> ());
+		Destroy (thePiecee.GetComponent<BoxCollider> ());
+		colorPieceDict = new Dictionary<ColorType, GameObject> ();
+		for (int i = 0; i < colorPieces.Length; i++) {
+			if (!colorPieceDict.ContainsKey (colorPieces [i].color)) {
+				colorPieceDict.Add (colorPieces [i].color, colorPieces [i].thePiece);
+			}
+		}
+	}
+
+	public void SetColor(ColorType newColor){
+		color = newColor;
+
+		if (colorPieceDict.ContainsKey (newColor)) {
+
+			thePiecee.GetComponent<Material>().color.Equals(colorPieceDict[newColor]);
+		}
+		//= colorPieceDict [newColor];
+	}
+}
---
+++ Assets/Scripts/Luke Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	public float paddleSpeed = 1f;
+
+	public Vector3 playerPos = new Vector3(-46.86f, 3.0f, .61f);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float zPos = transform.position.z + (Input.GetAxis ("Horizontal") * paddleSpeed);
+		playerPos = new Vector3 (-46.86f, 3.02f, Mathf.Clamp (-4.33f, 5.33f, zPos));
+		transform.position = playerPos;
+	}
+}
