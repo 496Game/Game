81a32ee099204a3db8fbe2b3cccc862e12f840e1
Luke Lamberg <lukelamberg@Lukes-MacBook-Air.local>
2018-05-08 14:33:02 -0500

    Hopefully?
    gaben plz

--- Assets/Scripts/Luke Scripts/DeadZone.cs
+++ Assets/Scripts/Luke Scripts/DeadZone.cs
@@ -6,5 +6,7 @@ public class DeadZone : MonoBehaviour {
 	void OnTriggerEnter (Collider col)
 	{
 		GM.instance.LoseLife();
+		Destroy (GameObject.FindGameObjectWithTag ("Ball"));
+		print("Ball hit");
 	}
 }
